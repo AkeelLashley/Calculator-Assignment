bd199426b730ecc1e476671c43a3de36f478a5be
AkeelLashley <133791297+AkeelLashley@users.noreply.github.com>
2023-08-09 10:28:41 -0400

    Add files via upload

---
+++ script.js
@@ -0,0 +1,9 @@
+// Create 6 variables whichs grabs the document element id for each elemnt needed to create the calculator
+
+// Create four functions: add(), subtract(), divide(), multiply()
+// Call the correct function when the user clicks on one of the buttons
+// Perform the given calculation using num1 and num2
+// Render the result of the calculation in the paragraph in the index.html
+
+// E.g. if the user enter 8 in the first input field and 2 in the second input field then clicks on the "Plus" button, it should render
+// "Sum: 10" (since 8 + 2 = 10) inside the paragraph index.html
